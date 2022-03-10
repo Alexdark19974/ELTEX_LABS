e1303c6028d39d92ba66b4ab8803011bcaafe2a6
Alexanderpetrov <voitexman11@gmail.com>
2022-03-10 11:23:35 +0300

    completed adress_book

--- LAB_4/Address_book/address_book.c
+++ LAB_4/Address_book/address_book.c
@@ -88,34 +88,149 @@ void address_book(void)
             {
                 getchar();
 
-                char find_name[50] = {0};
-                int found_name = -1;
+                enum{SEARCH_BY_NAME = '1', SEARCH_BY_MIDDLE_NAME, SEARCH_BY_LAST_NAME, SEARCH_BY_PHONE_NUMBER};
 
-                printf("Enter the name to be found: ");
+                puts("Press 1 to search by name, press 2 - to search by middle name, press 3 to search by last name, press 4 to search by phone number: ");
 
-                fgets(find_name, sizeof(find_name), stdin);
-
-                for (int i = 0; i != counter; i++)
+                switch (getchar())
                 {
-                    if ((found_name = (strcmp(file[i].name, find_name))) == 0)
+                    case SEARCH_BY_NAME:
                     {
-                        printf("1) Name: %s", file[i].name);
-                        printf("2) Middle name: %s", file[i].middle_name);
-                        printf("3) Last name: %s", file[i].last_name);
-                        printf("4) Phone number: %s", file[i].number);
+                        getchar();
+
+                        char find_name[50] = {0};
+                        int found_name = -1;
+
+                        printf("Enter the name to be found: ");
+
+                        fgets(find_name, sizeof(find_name), stdin);
+
+                        for (int i = 0; i != counter; i++)
+                        {
+                            if ((found_name = (strcmp(file[i].name, find_name))) == 0)
+                            {
+                                printf("1) Name: %s", file[i].name);
+                                printf("2) Middle name: %s", file[i].middle_name);
+                                printf("3) Last name: %s", file[i].last_name);
+                                printf("4) Phone number: %s", file[i].number);
+                                break;
+                            }
+                        }
+                        
+                        if (found_name != 0)
+                        {
+                            puts("The name not found.");
+                        }
+                        else
+                        {
+                            puts("Successfully found.");
+                        }
+
+                        break;
+                    }
+                    case SEARCH_BY_MIDDLE_NAME:
+                    {
+                        getchar();
+
+                        char find_middle_name[50] = {0};
+                        int found_middle_name = -1;
+                        
+                        printf("Enter the middle name to be found: ");
+
+                        fgets(find_middle_name, sizeof(find_middle_name), stdin);
+
+                        for (int i = 0; i != counter; i++)
+                        {
+                            if ((found_middle_name = (strcmp(file[i].middle_name, find_middle_name))) == 0)
+                            {
+                                printf("1) Name: %s", file[i].name);
+                                printf("2) Middle name: %s", file[i].middle_name);
+                                printf("3) Last name: %s", file[i].last_name);
+                                printf("4) Phone number: %s", file[i].number);
+                                break;
+                            }
+                        }
+                        
+                        if (found_middle_name != 0)
+                        {
+                            puts("The middle name not found.");
+                        }
+                        else
+                        {
+                            puts("Successfully found.");
+                        }
+
+                        break;
+                    }
+                    case SEARCH_BY_LAST_NAME:
+                    {
+                        getchar();
+
+                        char find_last_name[50] = {0};
+                        int found_last_name = -1;
+                        
+                        printf("Enter the middle name to be found: ");
+
+                        fgets(find_last_name, sizeof(find_last_name), stdin);
+
+                        for (int i = 0; i != counter; i++)
+                        {
+                            if ((found_last_name = (strcmp(file[i].last_name, find_last_name))) == 0)
+                            {
+                                printf("1) Name: %s", file[i].name);
+                                printf("2) Middle name: %s", file[i].middle_name);
+                                printf("3) Last name: %s", file[i].last_name);
+                                printf("4) Phone number: %s", file[i].number);
+                                break;
+                            }
+                        }
+                        
+                        if (found_last_name != 0)
+                        {
+                            puts("The last name not found.");
+                        }
+                        else
+                        {
+                            puts("Successfully found.");
+                        }
+
+                        break;
+                    }
+                    case SEARCH_BY_PHONE_NUMBER:
+                    {
+                        getchar();
+
+                        char find_phone_number[50] = {0};
+                        int found_phone_number = -1;
+                        
+                        printf("Enter the phone number name to be found: ");
+
+                        fgets(find_phone_number, sizeof(find_phone_number), stdin);
+
+                        for (int i = 0; i != counter; i++)
+                        {
+                            if ((found_phone_number = (strcmp(file[i].number, find_phone_number))) == 0)
+                            {
+                                printf("1) Name: %s", file[i].name);
+                                printf("2) Middle name: %s", file[i].middle_name);
+                                printf("3) Last name: %s", file[i].last_name);
+                                printf("4) Phone number: %s", file[i].number);
+                                break;
+                            }
+                        }
+                        
+                        if (found_phone_number != 0)
+                        {
+                            puts("The phone number not found.");
+                        }
+                        else
+                        {
+                            puts("Successfully found.");
+                        }
+
                         break;
                     }
                 }
-
-                if (found_name == -1)
-                {
-                    puts("The name not found.");
-                }
-                else
-                {
-                    puts("Successfully found.");
-                }
-                break;
             }
             case DELETE_CUSTOMER:
             {
